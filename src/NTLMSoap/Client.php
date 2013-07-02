1c38740035e4d5aeed6b2d93e03304606dec71d8
Niels Stampe <niels@keymade.io>
2013-07-02 23:13:39 +0200

    remove stuff

--- src/NTLMSoap/Client.php
+++ src/NTLMSoap/Client.php
@@ -6,10 +6,11 @@ use \Psr\Log\LoggerAwareInterface;
 use \Psr\Log\LoggerAwareTrait;
 use \Psr\Log\LoggerInterface;
 
-class Client extends \SoapClient{
+class Client extends \SoapClient
+{
 	use LoggerAwareTrait;
 	
-	private $options	= Array();
+	private $options = Array();
 	
 	/**
 	 * 
@@ -18,31 +19,37 @@ class Client extends \SoapClient{
 	 * @param \Psr\Log\LoggerAwareInterface $logger
 	 * @see \SoapClient::__construct()
 	 */
-	public function __construct($url, $data, LoggerInterface $logger = null){
-		if($logger){
+	public function __construct($url, $data, LoggerInterface $logger = null)
+	{
+		if ($logger)
+		{
 			$this->setLogger($logger);
 		}
 		
 		$this->options = $data;
 		
-		if(empty($data['ntlm_username']) && empty($data['ntlm_password'])){
+		if (empty($data['ntlm_username']) && empty($data['ntlm_password']))
+		{
 			parent::__construct($url, $data);
-		}else{
-			$this->use_ntlm				= true;
-			HttpStream\NTLM::$user		= $data['ntlm_username'];
+		}
+		else
+		{
+			$this->use_ntlm            = true;
+			HttpStream\NTLM::$user     = $data['ntlm_username'];
 			HttpStream\NTLM::$password = $data['ntlm_password'];
 			
 			stream_wrapper_unregister('http');
-			if(!stream_wrapper_register('http', '\\NTLMSoap\\HttpStream\\NTLM')){
+			if (!stream_wrapper_register('http', '\\NTLMSoap\\HttpStream\\NTLM'))
+			{
 				throw new Exception("Unable to register HTTP Handler");
 			}
 			
 			$time_start = microtime(true);
 			parent::__construct($url, $data);
 			
-			if(!empty($this->logger) && (($end_time = microtime(true) - $time_start) > 0.1)){
-				$this->logger->debug("WSDL Timer", Array("time" => $end_time, "url" => $url));
-			}
+			// if(!empty($this->logger) && (($end_time = microtime(true) - $time_start) > 0.1)){
+			// 	$this->logger->debug("WSDL Timer", Array("time" => $end_time, "url" => $url));
+			// }
 			
 			stream_wrapper_restore('http');
 		}
@@ -53,10 +60,11 @@ class Client extends \SoapClient{
 	 * (non-PHPdoc)
 	 * @see SoapClient::__doRequest()
 	 */
-	public function __doRequest($request, $location, $action, $version, $one_way=0) {
-		$this->__last_request	= $request;
-		$start_time				= microtime(true);
-
+	public function __doRequest($request, $location, $action, $version, $one_way = 0)
+	{
+		$this->__last_request = $request;
+		$start_time           = microtime(true);
+		
 		$ch = curl_init($location);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		
@@ -64,34 +72,35 @@ class Client extends \SoapClient{
 			'Method: POST',
 			'User-Agent: PHP-SOAP-CURL',
 			'Content-Type: text/xml; charset=utf-8',
-			'SOAPAction: "' . $action . '"',
+			'SOAPAction: "' . $action . '"'
 		));
 		
 		curl_setopt($ch, CURLOPT_POST, true);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
 		curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-
-		if(!empty($this->options['ntlm_username']) && !empty($this->options['ntlm_password'])){
-			curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_NTLM);
-			curl_setopt($ch, CURLOPT_USERPWD, $this->options['ntlm_username'].':'. $this->options['ntlm_password']);
-		}
-
-		$response = curl_exec($ch);
-
-		// Log as an error if the curl call isn't a success
-		$http_status	= curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		$log_func = $http_status == 200 ? 'debug' : 'error';
 		
-		// Log the call
-		$this->logger->$log_func("SoapCall: " . $action, Array(
-			"Location"				=> $location,
-			"HttpStatus"			=> $http_status,
-			"Request"				=> $request,
-			"Response"				=> $response,
-			"RequestTime"			=> curl_getinfo($ch, CURLINFO_TOTAL_TIME),
-			"RequestConnectTime"		=> curl_getinfo($ch, CURLINFO_CONNECT_TIME),
-			"Time"				=> microtime(true) - $start_time
-		));
+		if (!empty($this->options['ntlm_username']) && !empty($this->options['ntlm_password']))
+		{
+			curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_NTLM);
+			curl_setopt($ch, CURLOPT_USERPWD, $this->options['ntlm_username'] . ':' . $this->options['ntlm_password']);
+		}
+		
+		$response = curl_exec($ch);
+		
+		// Log as an error if the curl call isn't a success
+		// $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		// $log_func    = $http_status == 200 ? 'debug' : 'error';
+		
+		// // Log the call
+		// $this->logger->$log_func("SoapCall: " . $action, Array(
+		// 	"Location"				=> $location,
+		// 	"HttpStatus"			=> $http_status,
+		// 	"Request"				=> $request,
+		// 	"Response"				=> $response,
+		// 	"RequestTime"			=> curl_getinfo($ch, CURLINFO_TOTAL_TIME),
+		// 	"RequestConnectTime"		=> curl_getinfo($ch, CURLINFO_CONNECT_TIME),
+		// 	"Time"				=> microtime(true) - $start_time
+		// ));
 		
 		return $response;
 	}
